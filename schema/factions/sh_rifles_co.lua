7bc07e6816952b95e26d7840b7c86247ef33cb19
MonkeyNut <38351032+TeamMonkeyNut@users.noreply.github.com>
2018-04-17 12:51:39 +0100

    Create sh_rifles_co.lua

---
+++ schema/factions/sh_rifles_co.lua
@@ -0,0 +1,20 @@
+FACTION.name = "1st Battalion The Rifles CO"
+FACTION.desc = "1st Battalion The Rifles Rifles Commissioned Officer"
+FACTION.color = Color(0, 0, 255)
+FACTION.isDefault = false
+FACTION.models = {
+	"models/player/Group01/male_02.mdl"
+}
+FACTION.armor = 0
+FACTION.maxhealth = 100
+FACTION.health = 100
+
+function FACTION:onSpawn(client)
+		client:SetArmor(self.armor) -- Sets armour
+		client:SetMaxHealth(self.maxhealth) -- Sets maxhealth, that means the health you can be healed to.
+		client:SetHealth(self.health) -- Sets your health, you can not be healed to this amount unless your maxhealth is the same. This is needed because gmod sets your health to 100 by default.
+end
+
+FACTION.pay = 20
+FACTION.isGloballyRecognized = true
+FACTION_UK_RIFLES_CO = FACTION.index
